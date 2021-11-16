12319444d014c5727b1d78c7d6f76a57e00a267a
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2021-11-16 00:25:14 +0000

    [LeetCode Sync] Runtime - 44 ms (5.18%), Memory - 13.3 MB (18.23%)

---
+++ 0009-palindrome-number/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        xString = str(x)
+        if xString == xString[::-1]:
+            return True
+        else:
+            return False
+         
+        
