ea913200660758d940a45f060b7ca97ba0960ae4
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2023-03-22 20:38:58 +0000

    [LeetCode Sync] Runtime - 189 ms (5.01%), Memory - 21 MB (33.84%)

---
+++ 0238-product-of-array-except-self/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        otp = [1] * len(nums)
+
+        pretest = 1
+        for x in range(len(nums)):
+            otp[x] = pretest
+            pretest *= nums[x]
+        postest = 1
+        for y in range(len(nums) -1, -1, -1):
+            otp[y] *= postest
+            postest *= nums[y]
+        return(otp)
+    
+
+
