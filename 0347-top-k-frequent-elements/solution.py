468d9d27dfd9bba5651b915e5e7f57e2df3a914a
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2023-05-22 13:13:59 +0000

    [LeetCode Sync] Runtime - 1153 ms (5.03%), Memory - 16.5 MB (27.17%)

--- 0347-top-k-frequent-elements/solution.py
+++ 0347-top-k-frequent-elements/solution.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        hmap = {}
+        numset = {}
         for x in nums:
-            if not x in hmap:
-                hmap[x] = nums.count(x)
-        otp = []
-        sorted_hmap = sorted(hmap.items(), key=lambda x:x[1], reverse=True)
-        print(sorted_hmap)
-        #converted_hmap = dict(sorted_hmap)
-        #print(converted_hmap)
+            if not x in numset:
+                numset[x] = nums.count(x)
+        sortedset = []
+        sorted_hmap = sorted(numset.items(), key=lambda x:x[1], reverse=True)
         for i in range(k):
-            otp.append(sorted_hmap[i][0])
-        return (otp)
+            sortedset.append(sorted_hmap[i][0])
+        return sortedset
+            
