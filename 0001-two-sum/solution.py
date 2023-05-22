909db1d649a6f0c674643f6bb6d6f32d5d7c4843
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2023-05-22 12:44:22 +0000

    [LeetCode Sync] Runtime - 45 ms (42.86%), Memory - 14.1 MB (16.18%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -3,7 +3,8 @@ class Solution(object):
         hashmap = {}
         for i, x in enumerate(nums):
             if target - x in hashmap:
-                return[i, hashmap[target - x]]
+                return(i, hashmap[target - x])
             else:
                 hashmap[x] = i
+            
 
