baaff42ddf0644dddbb5a20dc3a8e9a53cefde11
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2023-07-17 02:46:37 +0000

    [LeetCode Sync] Runtime - 863 ms (5.16%), Memory - 22.9 MB (14.08%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def maxProfit(self, prices):
-        low = prices[0]
         profit = 0
-
+        lowest = prices[0]
         for x in prices:
-            if x < low:
-                low = x
-            profit = max(profit, x - low)
+            if x < lowest:
+                lowest = x
+            profit = max(profit, x - lowest)
+        
         return profit
+
+            
+
