724fd27980a6021b1fda0135a03e7c2b341abe80
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-03 05:40:52 +0000

    [LeetCode Sync] Runtime - 1 ms (61.62%), Memory - 22.1 MB (100.00%)

--- 0143-reorder-list/solution.py
+++ 0143-reorder-list/solution.py
@@ -5,29 +5,26 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head:
-            return
-        
-        curr = head
-        slow = curr
-        fast = curr
-        while fast != None and fast.next != None:
+        if not head: return
+        slow = head
+        fast = head
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        prev = None
-        curr = slow.next
+        second = slow.next
         slow.next = None
-
+        prev = None
+        curr = second
         while curr:
             temp = curr.next
             curr.next = prev
             prev = curr
             curr = temp
-        
         first = head
         second = prev
-        while second != None:
-            temp1, temp2 = first.next, second.next
+        while second:
+            temp1 = first.next 
+            temp2 = second.next
             first.next = second
             second.next = temp1
             first = temp1
