a2af5f001765cf9dfc8826bbcec42a3ab00162a9
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-03 02:42:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.5 MB (100.00%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -5,14 +5,15 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
-            if nums[l] <= nums[mid]:
+            
+            if nums[mid] >= nums[l]:
                 if nums[l] <= target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
             else:
-                if nums[mid] < target <= nums[r]:
+                if nums[r] >= target > nums[mid]:
                     l = mid + 1
-                else:
+                else: 
                     r = mid - 1
         return -1
