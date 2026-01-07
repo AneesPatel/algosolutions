b7ac93d7fa50cfac946454c665ae6477b0171c0d
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-07 23:11:23 +0000

    [LeetCode Sync] Runtime - 35 ms (13.53%), Memory - 19.3 MB (69.44%)

--- 0003-longest-substring-without-repeating-characters/solution.py
+++ 0003-longest-substring-without-repeating-characters/solution.py
@@ -7,7 +7,6 @@ class Solution:
             while s[r] in count:
                 count.remove(s[l])
                 l += 1
-            
             count.append(s[r])
             res = max(res, r - l + 1)
         return res
