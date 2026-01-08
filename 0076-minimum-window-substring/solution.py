efea9b6bf440b91895e908c8bc4d3d7246432f79
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-08 03:11:44 +0000

    [LeetCode Sync] Runtime - 69 ms (57.09%), Memory - 19.7 MB (37.50%)

--- 0076-minimum-window-substring/solution.py
+++ 0076-minimum-window-substring/solution.py
@@ -1,27 +1,34 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
-            return ""
-        if t == "": return ""
-        count, window = {}, {}
-        res, reslen = [-1, -1], float("infinity")
-        l = 0
-        for c in t:
-            count[c] = 1 + count.get(c, 0)
-        have, need,l  = 0, len(count), 0
-        for r in range(len(s)):
+        if not t or not s:
+            return "" # Fix: Return empty string, not False
             
+        count, window = {}, Counter(t)
+        res, resLen = [-1, -1], float("inf")
+        l = 0
+        have, need = 0, len(window)
+        
+        for r in range(len(s)):
             c = s[r]
-            window[c] = 1 + window.get(c, 0)
-            if c in count and window[c] == count[c]:
+            count[c] = 1 + count.get(c, 0)
+
+            if c in window and count[c] == window[c]:
                 have += 1
+            
             while have == need:
-                if (r - l + 1)< reslen:
+                # update our result
+                if (r - l + 1) < resLen:
                     res = [l, r]
-                    reslen = (r - l + 1)
-                window[s[l]] -= 1
-                if s[l] in count and window[s[l]] < count[s[l]]:
+                    resLen = (r - l + 1)
+                
+                # pop from the left of our window
+                # TODO 3: Decrement count[s[l]] (not remove)
+                count[s[l]] -= 1 
+                
+                # TODO 4: If s[l] in window and count[s[l]] < window[s[l]], decrement 'have'
+                if s[l] in window and count[s[l]] < window[s[l]]:
                     have -= 1
                 l += 1
+                
         l, r = res
-        return s[l:r+1] if reslen != float("infinity") else ""
+        return s[l : r+1] if resLen != float("inf") else ""
