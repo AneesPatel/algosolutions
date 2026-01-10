1b9cb13d85d8f2a4b7f812d613816432c5b556b1
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-10 03:27:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.7 MB (8.60%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsdict = {}
-        for index, val in enumerate(nums):
-            if target - val in numsdict:
-                return([index, numsdict[target-val]])
+        count = {}
+        for i, x in enumerate(nums):
+            if target - x in count:
+                return [i, count[target-x]]
             else:
-                numsdict[val] = index
-            
+                count[x] = i
+
+        return -1
