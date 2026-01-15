05e254d5e083cbaac6260c866d0700ad9cef9d1b
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-15 00:58:31 +0000

    [LeetCode Sync] Runtime - 238 ms (75.08%), Memory - 21.8 MB (33.63%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -1,24 +1,33 @@
 class Solution:
-    from collections import defaultdict
+    from collections import deque
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
         res = 0
         rowlen, collen = len(grid), len(grid[0])
+        directions = [(0,1), (0,-1), (1, 0), (-1, 0)]
+        def bfs(r1, c1):
+            
+            q = deque()
+            q.append((r1, c1))
+            if grid[r1][c1] == '1': 
+                grid[r1][c1] = '0'
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + r, dc + c
+                    if nr < 0 or nc < 0 or nr >= rowlen or nc >= collen:
+                        continue
+                    if grid[nr][nc] == '1':
+                        grid[nr][nc] = '0'
+                        q.append((nr, nc))
+
+
         
-        def dfs(r, c):
-            if r >= rowlen or c >= collen or r < 0 or c < 0 or grid[r][c] == '0':
-                return
-            if grid[r][c] == '1':
-                grid[r][c] = '0'
-                dfs(r + 1, c)
-                dfs(r - 1, c)
-                dfs(r, c + 1)
-                dfs(r, c - 1) 
-
-
-
-        for row in range(rowlen):
-            for col in range(collen):
-                if grid[row][col] == '1':
+        for r1 in range(rowlen):
+            for c1 in range(collen):
+                if grid[r1][c1] == '1':
                     res += 1
-                    dfs(row, col)
+                    bfs(r1, c1)
         return res
+
+
