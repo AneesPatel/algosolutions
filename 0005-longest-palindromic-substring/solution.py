452c876646d6560dca57a76ca494fa2a68c35257
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-01-25 21:20:09 +0000

    [LeetCode Sync] Runtime - 1765 ms (22.43%), Memory - 27.2 MB (7.55%)

--- 0005-longest-palindromic-substring/solution.py
+++ 0005-longest-palindromic-substring/solution.py
@@ -1,30 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s:
-            return ""
-        start = 0
-        end = 0
-        
-        # 1. Changed loop variable 'x' to 'i' to match usage below
-        for i in range(len(s)):
-            # 2. Added 'self.' to call the class method
-            len1 = self.expand(s, i, i)
-            len2 = self.expand(s, i, i + 1)
-            max_len = max(len1, len2)
-            
-            # The 'end - start' here tracks the length of the currently found max string
-            # (Note: standard solution usually tracks max_len separate from indices, 
-            # but this works if we treat end as inclusive index)
-            if max_len > end - start:
-                # 3. Logic Fix: Integer division (// 2) is required to find the offset
-                start = i - (max_len - 1) // 2
-                end = i + max_len // 2
-                
-        return s[start:end+1]
-    
-    def expand(self, s: str, l: int, r: int):
-        # 4. Fixed variable names: changed 'left'/'right' to 'l'/'r'
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return r - l - 1
+        n = len(s)
+        if n <= 1:
+            return s
+        dp = [[False] * n for _ in range(n)]
+        longest = ""
+        for x in range(n):
+            dp[x][x] = True
+            longest = s[0]
+        for length in range(2, n + 1):
+            for l in range(n - length + 1):
+                r = l + length - 1
+                if s[l] == s[r]:
+                    if length == 2 or dp[l + 1][r - 1]:
+                        dp[l][r] = True
+                        if length > len(longest):
+                            longest = s[l:r + 1]
+        return longest
