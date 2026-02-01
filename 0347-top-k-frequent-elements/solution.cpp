e04ee5ff6fe8add3161844437792f879cdb06d81
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-01 22:00:20 +0000

    [LeetCode Sync] Runtime - 3 ms (50.20%), Memory - 18 MB (38.70%)

---
+++ 0347-top-k-frequent-elements/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> q;
+        unordered_map<int, int> map = {};
+        for (int i = 0; i < nums.size(); i++){
+            if (map.count(nums[i])){
+                map[nums[i]] += 1;
+            }
+            else{
+                map[nums[i]] = 1;
+            }
+        }
+        for (auto i : map){
+            q.push({i.second, i.first});
+        }
+        vector<int> res = {};
+        while(res.size() != k){
+            int temp = q.top().second;
+            q.pop();
+            res.push_back(temp);
+        }
+        return res;
+
+    }
+};
