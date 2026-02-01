9d61607fb0d95bf129a500efe6c153406c42044c
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-01 07:15:18 +0000

    [LeetCode Sync] Runtime - 14 ms (74.27%), Memory - 25 MB (78.85%)

---
+++ 0049-group-anagrams/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map = {};
+        for (int i = 0; i < strs.size(); i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            if (map.count(temp)){
+                map[temp].push_back(strs[i]);
+            }
+            else{
+                map[temp] = {strs[i]};
+            }
+        }
+        vector<vector<string>> res = {};
+        for (const auto& [key, value] : map) {
+            res.push_back(value);
+        }
+        return res;
+
+    }
+};
