e2aa3c5d62702d52ae7f2235228431adec5c26f6
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-01 05:47:42 +0000

    [LeetCode Sync] Runtime - 49 ms (55.96%), Memory - 90.9 MB (44.45%)

---
+++ 0217-contains-duplicate/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> prev = {};
+        for(int i = 0; i < nums.size(); i++){
+            if (prev.count(nums[i])){
+                return true;
+            }
+            prev.insert(nums[i]);
+        }
+        return false;
+    }
+};
