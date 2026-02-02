71bc5bffcbe0b8b1016ce76c3eadb5a8f329c3e5
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-02 01:51:16 +0000

    [LeetCode Sync] Runtime - 3 ms (37.03%), Memory - 40.3 MB (42.54%)

---
+++ 0238-product-of-array-except-self/solution.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <vector>
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res(nums.size(), 1);
+        int prefix = 1;
+        int postfix = 1;
+        for (int i = 0; i < res.size(); i++){
+            res[i] = prefix;
+            prefix *= nums[i];            
+        }
+        for (int i = res.size() - 1; i >= 0; --i){
+            res[i] *= postfix;
+            postfix *= nums[i];
+            
+        }
+        return res;
+    }
+};
