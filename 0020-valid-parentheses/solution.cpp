80d372e8c9b6272393d0bf90558eabf577996909
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-02 21:15:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (38.37%)

---
+++ 0020-valid-parentheses/solution.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+#include <vector>
+#include <string>
+#include <stack>
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> para = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+        stack<char> stack = {};
+        for(char cur:s){
+            if (para.count(cur)){
+                stack.push(cur);
+            }
+            else{
+                if(stack.empty()){
+                    return false;
+                }
+                if (para[stack.top()] == cur){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+
+        }
+
+        if(stack.empty()){
+            return true;
+        }
+        return false;
+    }
+};
