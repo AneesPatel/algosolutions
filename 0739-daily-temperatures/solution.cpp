f7a26b8bd41fca602eef4faacb94b6d92c200b55
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-03 04:29:02 +0000

    [LeetCode Sync] Runtime - 27 ms (36.20%), Memory - 107.2 MB (52.75%)

---
+++ 0739-daily-temperatures/solution.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> prevs = {};
+        vector<int> res(temperatures.size(), 0);
+        
+        for(int i = 0; i < temperatures.size(); i++){
+            while (!prevs.empty() and temperatures[i] > temperatures[prevs.top()]){
+                res[prevs.top()] = (i - prevs.top());
+                prevs.pop();
+            }
+            prevs.push(i);
+        }
+        return res;
+    }
+};
