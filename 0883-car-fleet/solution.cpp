aa43b270a68f65c1d4eb975de454222b05772b18
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-03 19:04:29 +0000

    [LeetCode Sync] Runtime - 54 ms (42.99%), Memory - 109.1 MB (45.76%)

---
+++ 0883-car-fleet/solution.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+#include <vector>
+#include <string>
+#include <stack>
+#include <algorithm>
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
+        
+        for (int i = 0; i < position.size(); ++i) {
+            cars.push_back({position[i], speed[i]});
+        }
+        sort(cars.rbegin(), cars.rend());
+
+        stack<double> prevs = {};
+        for (int i{0}; i < position.size(); ++i){
+            double time = (double)(target - cars[i].first) / cars[i].second; 
+            if (prevs.empty() or time > prevs.top()){
+                prevs.push(time);
+            }
+        }
+        return prevs.size();
+
+
+    }
+};
