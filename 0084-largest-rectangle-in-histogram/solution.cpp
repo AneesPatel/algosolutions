7213db71b36bdc0a04fd89095b79b84231c85066
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-04 00:34:43 +0000

    [LeetCode Sync] Runtime - 27 ms (37.91%), Memory - 83.9 MB (49.41%)

---
+++ 0084-largest-rectangle-in-histogram/solution.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<pair<int, int>> prevs = {};
+        int longest = 0;
+        for (int i = 0; i < heights.size(); ++i){
+            while(!prevs.empty() and prevs.top().first > heights[i]){
+                int height = prevs.top().first;
+                prevs.pop();
+                int width;
+                if (prevs.empty()){
+                    width = i;
+                }
+                else{
+                    width = i - prevs.top().second - 1;
+                }
+                longest = max(longest, height * width);
+            }
+            prevs.push({heights[i], i});
+        }
+        while (!prevs.empty()){
+            int height = prevs.top().first;
+            prevs.pop();
+            int width;
+            if (prevs.empty()){
+                width = heights.size();
+            }
+            else{
+                width = heights.size() - prevs.top().second - 1;
+                longest = max(longest, height * width);
+            }
+            longest = max(longest, height * width);
+        }
+        return longest;
+    }
+};
