ac93ce6ee27a010ab0eb8219a5d1e1dd1f65c119
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-04 02:48:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.8 MB (77.13%)

---
+++ 0125-valid-palindrome/solution.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+        while (l < r){
+            while(l < r and not isalnum(s[l])){
+                l++;
+            }
+            while(l < r and not isalnum(s[r])){
+                r--;
+            }
+            if (std::tolower(s[l]) != std::tolower(s[r])){
+                return false;
+            }
+            else{
+                r--;
+                l++;
+            }
+        }
+        return true;
+    }
+};
