18f8d1ac45351e97aaa5492caac4e685867b00f8
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-02-07 05:23:47 +0000

    [LeetCode Sync] Runtime - 5 ms (44.44%), Memory - 10.8 MB (23.03%)

--- 0567-permutation-in-string/solution.cpp
+++ 0567-permutation-in-string/solution.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) return false;
-        
-        // Use vectors of size 26 for O(1) access (assuming lowercase English letters)
-        vector<int> s1Count(26, 0);
-        vector<int> s2Count(26, 0);
-        
-        // Frequency of s1 characters
-        for (char c : s1) {
-            s1Count[c - 'a']++;
+        int n = s1.size();
+        unordered_map<char, int> count = {};
+        unordered_map<char, int> count2 = {};
+        for(char cur:s1){
+            count[cur] += 1;
         }
-        
-        // Initial Window: Check the first s1.length() characters of s2
-        for (int i = 0; i < s1.length(); i++) {
-            s2Count[s2[i] - 'a']++;
-        }
-        
-        if (s1Count == s2Count) return true;
-        
-        // Sliding Window
         int l = 0;
-        for (int r = s1.length(); r < s2.length(); r++) {
-            // Add the new character (right side of window)
-            s2Count[s2[r] - 'a']++;
-            
-            // Remove the old character (left side of window)
-            s2Count[s2[l] - 'a']--;
-            l++;
-            
-            // Compare the frequency vectors
-            if (s1Count == s2Count) return true;
+        for (int r = 0; r < s2.size(); r++){
+            count2[s2[r]] += 1;
+            while (r - l + 1 > n){
+                count2[s2[l]] -= 1;
+                if (count2[s2[l]] == 0) {
+                    count2.erase(s2[l]);
+                }
+                l += 1;
+            }
+            if (count == count2){
+                return true;
+            }
+
         }
-        
         return false;
     }
 };
