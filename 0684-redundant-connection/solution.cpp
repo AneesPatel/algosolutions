aa65753f784dbddec5b8320ae860ab803184b7fb
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-03-17 20:36:38 +0000

    [LeetCode Sync] Runtime - 2 ms (35.62%), Memory - 13.3 MB (43.99%)

--- 0684-redundant-connection/solution.cpp
+++ 0684-redundant-connection/solution.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    bool dfs(int cur, int findNode, unordered_set<int>& prevs, unordered_map<int, vector<int>>& adjMap){
-        if(prevs.count(cur)){
+    int find(int node, vector<int>& parents){
+        if(node != parents[node]){
+            parents[node] = find(parents[node], parents);
+        }
+        return parents[node];
+    }
+    bool unionFind(int node1, int node2, vector<int>& parents){
+        int root1 = find(node1, parents);
+        int root2 = find(node2, parents);
+        if(root1 == root2){
             return false;
         }
-        if(cur == findNode){
-            return true;
-        }
-        prevs.insert(cur);
-        for(auto curAdj : adjMap[cur]){
-            if (dfs(curAdj, findNode, prevs, adjMap)){
-                return true;
-            }
-        }
-        return false;
+        parents[root1] = root2;
+
+        return true;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        unordered_set<int> prevs;
-        unordered_map<int, vector<int>> adjMap;
+        int n = edges.size();
+        vector<int> parents;
+        for(int i = 0; i < n + 1; ++i){
+            parents.push_back(i);
+        }
         for(auto curPair : edges){
-            //if starting at curpair 0 we can find curPair 1 it is a redundant
-            prevs.clear();
-            if (dfs(curPair[0], curPair[1], prevs, adjMap)){
-                return curPair;
+            if(!unionFind(curPair[0], curPair[1], parents)){
+                return (curPair);
             }
-            adjMap[curPair[0]].push_back(curPair[1]);
-            adjMap[curPair[1]].push_back(curPair[0]);
-            
         }
         return {};
-        
     }
 };
