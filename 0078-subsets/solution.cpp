a773cf3af109a1c2552eab6a51f472874c299ecb
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-03-25 06:54:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (95.37%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void backtrack(int index, vector<int>& path, const vector<int>& nums){
-        //vector<int> curpath = path;
-        res.push_back(path);
-
-        for (int i = index; i < nums.size(); ++i){
-            path.push_back(nums[i]);
-            backtrack(i + 1, path, nums);
-            path.pop_back();
-        }
+    void backtrack(int index, vector<vector<int>>& res, vector<int>& path, vector<int>& nums){
         
+        res.push_back(path);
+            
+        
+        for(int i = index; i < nums.size(); ++i){
+            path.push_back(nums[i]);
+            backtrack(i + 1, res, path, nums);
+            path.pop_back();
+
+        }
+
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        res.clear();
+        vector<vector<int>> res;
         vector<int> path;
-        backtrack(0, path, nums);
+        backtrack(0, res, path, nums);
         return res;
     }
 };
