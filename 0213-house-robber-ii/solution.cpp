6dd8a1fc28c2948bb66bdf7a85eb2d11da9851b5
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-04-01 03:01:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.9 MB (36.01%)

--- 0213-house-robber-ii/solution.cpp
+++ 0213-house-robber-ii/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int n;
-    int dp(int i, vector<int>& memo, vector<int>& nums){
-        if(i >= n){
+    int dp(int i, vector<int>& nums, vector<int>& memo){
+        if(i >= nums.size()){
             return 0;
         }
         if(memo[i] != -1){
             return memo[i];
         }
-        
-        memo[i] = max(nums[i] + dp(i + 2, memo, nums), dp(i + 1, memo, nums));
-        return memo[i]; 
-
+        memo[i] = max(nums[i] + dp(i + 2, nums, memo), dp(i + 1, nums, memo));
+        return memo[i];
     }
     int helper(vector<int>& nums){
-        n = nums.size();
-        vector<int> memo(n, -1);
-        return dp(0, memo, nums);
+        vector<int> memo(nums.size(), -1);
+        return dp(0, nums, memo);
     }
     int rob(vector<int>& nums) {
-        int n = nums.size();
         if(nums.size() == 1){
             return nums[0];
+        }
+        else if(nums.size() == 2){
+            return max(nums[1], nums[0]);
         }
         vector<int> nums1(nums.begin(), nums.end() - 1);
         vector<int> nums2(nums.begin() + 1, nums.end());
