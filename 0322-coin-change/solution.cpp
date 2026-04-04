67a82670e27d16f1fdc0df6aa7d4e010c65a0406
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-04-04 17:41:35 +0000

    [LeetCode Sync] Runtime - 19 ms (78.63%), Memory - 17.9 MB (80.58%)

--- 0322-coin-change/solution.cpp
+++ 0322-coin-change/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX - 1);
+        vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
-        for(int a = 1; a < amount + 1; ++a){
-            for(auto coin : coins){
-                if(a - coin >= 0){
-                    dp[a] = min(dp[a], dp[a - coin] + 1);
+        for(int i = 1; i < amount + 1; ++i){
+            for(auto cur : coins){
+                if(i >= cur){
+                    dp[i] = min(dp[i], dp[i - cur] + 1);
                 }
             }
         }
-        if (dp[amount] == INT_MAX - 1){
+        if(dp[amount] == amount +1){
             return -1;
         }
-
         return dp[amount];
     }
 };
