0543ae1bd9077560aa114bd2ecb4e46f943e6817
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-04-07 19:08:02 +0000

    [LeetCode Sync] Runtime - 81 ms (77.95%), Memory - 13.3 MB (92.15%)

--- 0416-partition-equal-subset-sum/solution.cpp
+++ 0416-partition-equal-subset-sum/solution.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum % 2 != 0) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        if(total % 2 == 1){
             return false;
         }
-
-        int target = sum / 2;
-        int n = nums.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] ||
-                               dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+        int target = total/2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for(auto cur : nums){
+            for(int i = target; i >= cur; --i){
+                dp[i] = dp[i] or dp[i - cur];
             }
         }
+        return dp[target];
 
-        return dp[n][target];
+        
     }
 };
