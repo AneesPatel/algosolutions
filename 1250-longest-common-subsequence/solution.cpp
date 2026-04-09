35dcfde3a43e3d283e765ddbd8c84b9499bbc0a3
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-04-09 20:48:25 +0000

    [LeetCode Sync] Runtime - 51 ms (6.15%), Memory - 27.5 MB (26.83%)

--- 1250-longest-common-subsequence/solution.cpp
+++ 1250-longest-common-subsequence/solution.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int dp(int i, int j, string& text1, string& text2, vector<vector<int>>& memo){
+    int dfs(int i, int j, string& text1, string& text2, vector<vector<int>>& memo){
         if(i >= text1.size() or j >= text2.size()){
             return 0;
         }
         if(memo[i][j] != -1){
             return memo[i][j];
         }
-        
         if(text1[i] == text2[j]){
-            memo[i][j] = dp(i + 1, j + 1, text1, text2, memo) + 1;
+            memo[i][j] = dfs(i + 1, j + 1, text1, text2, memo) + 1;
         }
         else{
-            memo[i][j] = max(dp(i + 1, j, text1, text2, memo), dp(i, j + 1, text1, text2, memo));
+            memo[i][j] = max(dfs(i + 1, j, text1, text2, memo), dfs(i, j + 1, text1, text2, memo));
         }
-        
         return memo[i][j];
-
     }
     int longestCommonSubsequence(string text1, string text2) {
         vector<vector<int>> memo(text1.size(), vector<int>(text2.size(), -1));
-        //memo[i][j] = longest substring so far at index;
-        return dp(0, 0, text1, text2, memo);
+        return dfs(0, 0, text1, text2, memo);
     }
 };
