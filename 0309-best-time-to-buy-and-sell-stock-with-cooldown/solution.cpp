cb02ef65be90b825cfd9f0c81d65da91072fdf29
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-04-12 20:43:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.1 MB (76.04%)

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int dp(int i, int j, vector<vector<int>>& memo, vector<int>& prices){
-        if(j >= prices.size() or i >= prices.size()){
-            return 0;
-        }
-        if(memo[i][j] != -1){
-            return memo[i][j];
-        }
-        int cur_profit = prices[j] - prices[i];
-        
-        memo[i][j] = max(cur_profit + dp(j + 2, j + 3, memo, prices), dp(j, j + 1, memo, prices));
-        memo[i][j] = max(memo[i][j], dp(i, j + 1, memo, prices));
-        return memo[i][j];
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // dp[i][j] = max path profit;
-        vector<vector<int>> memo(n + 1, vector<int>(n + 1, -1));
-        return dp(0, 1, memo, prices);
-
+        vector<pair<int, int>> dp(n + 2, {0, 0});
+        dp[0] = {-prices[0], 0};
+        //dp[maxbuy, maxsell]
+        for(int i = 1; i < n; ++i){
+            int buyProfit = 0 - prices[i];
+            if(i - 2 >= 0){
+                buyProfit = dp[i - 2].second - prices[i];
+            }
+            int buyContinue = dp[i - 1].first;
+            dp[i].first = max(buyProfit, buyContinue);
+            
+            int sellProfit = dp[i - 1].first + prices[i];
+            int sellContinue = dp[i - 1].second;
+            
+            dp[i].second = max(sellProfit, sellContinue);
+        }
+        return dp[n - 1].second;
     }
 };
