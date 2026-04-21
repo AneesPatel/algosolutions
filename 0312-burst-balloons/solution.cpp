1a20c631fc247d60175bac49a9e9a72eedcf1638
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-04-21 23:56:33 +0000

    [LeetCode Sync] Runtime - 211 ms (16.11%), Memory - 13.8 MB (79.79%)

--- 0312-burst-balloons/solution.cpp
+++ 0312-burst-balloons/solution.cpp
@@ -9,7 +9,7 @@ public:
         }
         int temp = 0;
         for(int i = l; i <= r; ++i){
-            temp = max(temp, dp(l, i - 1, nums, memo) + (nums[l - 1] * nums[r + 1] * nums[i]) + dp(i + 1, r, nums, memo));
+            temp = max(temp, dp(l, i - 1, nums, memo) + (nums[l - 1] * nums[i] * nums[r + 1]) + dp(i + 1, r, nums, memo));
         }
         memo[l][r] = temp;
         return memo[l][r];
@@ -18,6 +18,6 @@ public:
         vector<vector<int>> memo(nums.size() + 1, vector<int>(nums.size() + 1, -1));
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        return dp(1, nums.size() - 2, nums, memo);
+        return dp(1, nums.size() -2, nums, memo);
     }
 };
