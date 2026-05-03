c6adc0f7b67c0d606a28f58fc689f7b2b25c291f
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-03 21:45:23 +0000

    [LeetCode Sync] Runtime - 441 ms (51.66%), Memory - 636.5 MB (15.57%)

--- 0211-design-add-and-search-words-data-structure/solution.cpp
+++ 0211-design-add-and-search-words-data-structure/solution.cpp
@@ -1,4 +1,4 @@
-class TrieNode{
+class TrieNode {
     public:
     vector<TrieNode*> nei;
     bool isWord;
@@ -11,17 +11,17 @@ class WordDictionary {
 public:
     TrieNode* root;
     WordDictionary() {
-        root = new TrieNode();
+        root = new TrieNode();   
     }
-    
+
     void addWord(string word) {
         TrieNode* node = root;
-        for(const char& cur : word){
+        for(char& cur : word){
             int index = cur - 'a';
             if(node->nei[index] == nullptr){
                 node->nei[index] = new TrieNode();
             }
-            node = node->nei[index];
+            node = node->nei[index]; 
         }
         node->isWord = true;
     }
@@ -34,20 +34,19 @@ public:
         if(index >= word.size()){
             return node->isWord;
         }
-        bool res = false;
         if(word[index] == '.'){
             for(int i = 0; i < 26; ++i){
                 if(node->nei[i] != nullptr and dfs(node->nei[i], index + 1, word)){
                     return true;
                 }
             }
-        }else{
-            if(node->nei[word[index] - 'a'] != nullptr){
-                return dfs(node->nei[word[index] - 'a'], index + 1, word);
+        } else{
+            int temp = word[index] - 'a';
+            if(node->nei[temp] != nullptr){
+                return dfs(node->nei[temp], index + 1, word);
             }
         }
         return false;
-
     }
 };
 
