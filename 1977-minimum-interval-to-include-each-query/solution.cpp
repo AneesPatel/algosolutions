7f6909a40a96c87b5d8e237aa9bbcd45bdd0624d
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-03 19:02:53 +0000

    [LeetCode Sync] Runtime - 81 ms (69.64%), Memory - 117.2 MB (76.00%)

--- 1977-minimum-interval-to-include-each-query/solution.cpp
+++ 1977-minimum-interval-to-include-each-query/solution.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        vector<int> res(queries.size());
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minq;
-        // size, rightindex
+        vector<int> res(queries.size(), -1);
         vector<pair<int, int>> nqueries;
+        //<val, orignal index>
         for(int i = 0; i < queries.size(); ++i){
             nqueries.push_back({queries[i], i});
         }
-        int i = 0;
         sort(nqueries.begin(), nqueries.end());
         sort(intervals.begin(), intervals.end());
-        for(auto q : nqueries){
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minq;
+        //minq = pair<size, rightvalue>
+        int i = 0;
+        for(auto& q : nqueries){
             int val = q.first;
             int index = q.second;
-            while(i < intervals.size() and val >= intervals[i][0]){
+            //populate while loop
+            while(i < intervals.size() and intervals[i][0] <= val){
                 minq.push({intervals[i][1] - intervals[i][0] + 1, intervals[i][1]});
                 i++;
             }
-            while(!minq.empty() and minq.top().second < q.first){
+            //delete while loop
+            while(!minq.empty() and val > minq.top().second){
                 minq.pop();
             }
             if(!minq.empty()){
                 res[index] = minq.top().first;
-            }
-            else{
-                res[index] = -1;
             }
         }
         return res;
