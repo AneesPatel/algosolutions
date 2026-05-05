ed77e711a138e612880f9557d61b332fd37cb0d5
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-05 20:38:34 +0000

    [LeetCode Sync] Runtime - 399 ms (49.14%), Memory - 20.8 MB (17.17%)

--- 0212-word-search-ii/solution.cpp
+++ 0212-word-search-ii/solution.cpp
@@ -15,60 +15,63 @@ class Trie{
         Trie(){
             root = new TrieNode();
         }
-    void addWord(string word, int i){
-        TrieNode* node = root;
-        for(char& cur : word){
-            int index = cur - 'a';
-            if(node->nei[index] == nullptr){
-                node->nei[index] = new TrieNode();
+        void addWord(string word, int index){
+            TrieNode* node = root;
+            for(char& cur : word){
+                int i = cur - 'a';
+                if(node->nei[i] == nullptr){
+                    node->nei[i] = new TrieNode();
+                }
+                node = node->nei[i];
             }
-            node = node->nei[index];
+            node->isWord = true;
+            node->wordIndex = index;
         }
-        node->isWord = true;
-        node->wordIndex = i;
-    }
 };
 class Solution {
 public:
-    int ROW; int COL;
-    void dfs(int r, int c, TrieNode* node, vector<vector<char>>& board, vector<string>& res, vector<string>& words){
+    int ROW;
+    int COL;
+    void dfs(int r, int c, TrieNode* node, vector<string>& res, vector<vector<char>>& board, vector<string>& words){
         if(node->isWord == true){
             res.push_back(words[node->wordIndex]);
             node->isWord = false;
         }
         char temp = board[r][c];
         board[r][c] = '#';
-        static const vector<vector<int>> directions = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-        for(const auto& dire : directions){
+        static const vector<vector<int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        for(auto& dire : directions){
             int nr = r + dire[0];
             int nc = c + dire[1];
-            if(nr < 0 or nc < 0 or nr >= ROW or nc >= COL or board[nr][nc] == '#'){
+            if(nr >= ROW or nc >= COL or nr < 0 or nc < 0 or board[nr][nc] == '#'){
                 continue;
             }
-            int index = board[nr][nc] - 'a';
-            if(node->nei[index] != nullptr){
-                dfs(nr, nc, node->nei[index], board, res, words);
+
+            if(node->nei[board[nr][nc] - 'a'] != nullptr){
+                dfs(nr, nc, node->nei[board[nr][nc] - 'a'], res, board, words);
             }
         }
         board[r][c] = temp;
     }
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie* trie = new Trie();
-        for(int i = 0; i < words.size(); ++i){
-            string word = words[i];
-            trie-> addWord(word, i);
-        }
         vector<string> res;
         ROW = board.size();
         COL = board[0].size();
-        for(int r = 0; r  < ROW; ++r){
+
+        Trie* trie = new Trie();
+
+        for(int i = 0; i < words.size(); ++i){
+            trie->addWord(words[i], i);
+        }
+        for(int r = 0; r < ROW; ++r){
             for(int c = 0; c < COL; ++c){
                 int index = board[r][c] - 'a';
                 if(trie->root->nei[index] != nullptr){
-                    dfs(r, c, trie->root->nei[index], board, res, words);
+                    dfs(r, c, trie->root->nei[index], res, board, words);
                 }
             }
         }
         return res;
+
     }
 };
