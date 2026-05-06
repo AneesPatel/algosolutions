02ec046bc6edfb7b1b65e76e77e0c13aea3accad
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-06 07:03:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.4 MB (61.59%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevs = {}
         for i in range(len(nums)):
-            curval = nums[i]
-            for j in range(i + 1, len(nums)):
-                curval2 = nums[j]
-                if(curval + curval2 == target):
-                    return ([i, j])
-        
+            goal = target - nums[i]
+            if goal in prevs:
+                return [i, prevs[goal]]
+            else:
+                prevs[nums[i]] = i
+        return -1
