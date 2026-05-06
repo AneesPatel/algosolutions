32f0136f5ef2a67df05127934796857a6d31356c
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-06 20:58:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (71.98%)

--- 0190-reverse-bits/solution.cpp
+++ 0190-reverse-bits/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    uint32_t reverseBits(uint32_t n) {
-        uint32_t ans = 0;
-        for (int i = 0; i < 32; i++) {
-            ans <<= 1;
-            ans |= (n & 1);
-            n >>= 1;
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; ++i){
+            res = res << 1;
+            res = res | (n & 1);
+            
+            n = n >> 1;
         }
-        return ans;
+        return res;
     }
 };
