9e9e77e7a8caae576be00ad675639dc020953266
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-08 20:21:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.4 MB (97.14%)

--- 0338-counting-bits/solution.cpp
+++ 0338-counting-bits/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res(n + 1, 0);
-        int MSB = 1;
+        vector<int> dp(n + 1, 0);
+        int msb = 1;
         for(int i = 1; i <= n; ++i){
-            if(MSB * 2 == i){
-                MSB = i;
+            if(msb * 2 == i){
+                msb = i;
             }
-            res[i] = 1 + res[i - MSB];
-            
+            dp[i] = 1 + dp[i - msb];
         }
-        return res;
+        return dp;
     }
 };
