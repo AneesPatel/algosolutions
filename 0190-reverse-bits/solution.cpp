4ee9313ac64feaa6a8f599718e7195966024f614
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-08 04:32:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (71.88%)

--- 0190-reverse-bits/solution.cpp
+++ 0190-reverse-bits/solution.cpp
@@ -5,8 +5,7 @@ public:
         for(int i = 0; i < 32; ++i){
             res = res << 1;
             res = res | (n & 1);
-            
-            n = n >> 1;
+            n =  n >> 1;
         }
         return res;
     }
