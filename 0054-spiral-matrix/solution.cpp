88774e4a4eabda3a0bd6cb85620c641e13d0a623
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-11 00:23:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (80.38%)

--- 0054-spiral-matrix/solution.cpp
+++ 0054-spiral-matrix/solution.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int ROW;
-    int COL;
+    const std::vector<std::vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int ROW; int COL;
     int SIZE;
-    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    void dfs(int r, int c, int curdire, vector<int>& res, vector<vector<int>>& matrix){
+    void dfs(int r, int c, int curDire, std::vector<int>& res, vector<vector<int>>& matrix){
         if(res.size() == SIZE){
             return;
         }
         res.push_back(matrix[r][c]);
         matrix[r][c] = INT_MIN;
-        int nr = r + directions[curdire][0];
-        int nc = c + directions[curdire][1];
-        if(nr < 0 or nc < 0 or nr >= ROW or nc >= COL or matrix[nr][nc] == INT_MIN){
-            curdire = (1 + curdire) % 4;
-            nr = r + directions[curdire][0];
-            nc = c + directions[curdire][1]; 
+        int nr = r + directions[curDire][0];
+        int nc = c + directions[curDire][1];
+        if(nr < 0 or nr >= ROW or nc < 0 or nc >= COL or matrix[nr][nc] == INT_MIN){
+            curDire = (curDire + 1) % 4;
+            nr = r + directions[curDire][0];
+            nc = c + directions[curDire][1];
         }
-        dfs(nr, nc, curdire, res, matrix);
-
+        dfs(nr, nc, curDire, res, matrix);
+         
     }
-
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
+        std::vector<int> res;
         ROW = matrix.size();
         COL = matrix[0].size();
         SIZE = ROW * COL;
