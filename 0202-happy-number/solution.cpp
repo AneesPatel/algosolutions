aaee3c16b39bd5d9a14e095e979f5dc7c20eacc3
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-13 00:59:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.4 MB (37.76%)

--- 0202-happy-number/solution.cpp
+++ 0202-happy-number/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    bool dfs(int n, unordered_set<int>& prevs){
+    bool dfs(int n, std::unordered_set<int>& prevs){
         if(n == 1){
             return true;
         }
-        
-        int total = 0;
-        while(n > 0){
-            total += (n % 10) * (n % 10);
-            n = n/10;
-        }
-        
-        if(prevs.count(total)){
+        if(prevs.count(n)){
             return false;
         }
-        prevs.insert(total);
-        return dfs(total, prevs);
+        prevs.insert(n);
+        int res = 0;
+        while(n > 0){
+            res += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        
+        return dfs(res, prevs);
     }
     bool isHappy(int n) {
-        unordered_set<int> prevs;
+        std::unordered_set<int> prevs;
         return dfs(n, prevs);
-    }
+    }    
 };
