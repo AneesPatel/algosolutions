fe015eaa457debe8f131bca0fe0955617a7fd1f6
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-13 06:38:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (42.76%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (x == 0) return 0;
-        if (n == 0) return 1;
-
+        if(x == 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
         double res = 1;
         long power = abs((long)n);
-
-        while (power) {
-            if (power & 1) {
+        while(power){
+            if(power & 1){
                 res *= x;
             }
             x *= x;
-            power >>= 1;
+            power = power >> 1;
         }
-
-        return n >= 0 ? res : 1 / res;
+        if(n < 0){
+            return 1/res;
+        }
+        return res;
     }
 };
