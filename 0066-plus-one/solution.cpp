15f76f084ea16b4b375e6c2caa53ccb75dfc0b38
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-13 05:25:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.5 MB (86.23%)

--- 0066-plus-one/solution.cpp
+++ 0066-plus-one/solution.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        int carry = 0;
         digits[n - 1] += 1;
-
+        int carry = 0;
         for(int i = n - 1; i >= 0; --i){
             digits[i] += carry;
-            carry = 0;
             if(digits[i] < 10){
                 return digits;
             }
-            if(digits[i] > 9){
-                carry = 1;
-                digits[i] = digits[i] - 10;
-            }
-            
+            digits[i] -= 10;
+            carry = 1;
         }
-        if(carry == 1){
+        if(carry > 0){
             digits.insert(digits.begin(), 1);
         }
         return digits;
