e0884c6e0728bf4d5e8fff6c51ff2c0142b13432
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-17 07:03:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 112.5 MB (60.17%)

--- 0134-gas-station/solution.cpp
+++ 0134-gas-station/solution.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int totalgas = accumulate(gas.begin(), gas.end(), 0);
-        int totalcost = accumulate(cost.begin(), cost.end(), 0);
+        int cur = 0;
+        int res = 0;
+        int totalgas = 0;
+        int totalcost = 0;
+        for(int i = 0; i < gas.size(); i++){
+            totalgas += gas[i];
+            totalcost += cost[i];
+        }
         if(totalgas < totalcost){
             return -1;
         }
-
-        int cur = 0;
-        int res = 0;
         for(int i = 0; i < gas.size(); ++i){
-            cur += gas[i] - cost[i];
+            cur = cur + gas[i] - cost[i];
             if(cur < 0){
                 cur = 0;
                 res = i + 1;
             }
+        }
+        if(res == gas.size()){
+            return - 1;
         }
         return res;
     }
