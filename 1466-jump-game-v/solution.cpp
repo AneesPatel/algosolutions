8e8ab0533a5cebf59f9da430661d8591fd00db4a
AneesPatel <91919134+AneesPatel@users.noreply.github.com>
2026-05-24 20:16:09 +0000

    [LeetCode Sync] Runtime - 7 ms (94.02%), Memory - 19.3 MB (30.90%)

--- 1466-jump-game-v/solution.cpp
+++ 1466-jump-game-v/solution.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
-    int n;
-
-    int dfs(int i,vector<int>& arr,int d,vector<int>& dp) {
-
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-
-        int ans = 1;
-
-        // move right
-        for(int j = i + 1;j <= min(n - 1, i + d);j++) {
-            // blocked
-            if(arr[j] >= arr[i]) {
-                break;
-            }
-
-            ans = max(ans,1 + dfs(j, arr, d, dp));
-        }
-
-        // move left
-        for(int j = i - 1;j >= max(0, i - d);j--) {
-            // blocked
-            if(arr[j] >= arr[i]) {
-                break;
-            }
-
-            ans = max(ans,1 + dfs(j, arr, d, dp));
-        }
-
-        return dp[i] = ans;
-    }
-
     int maxJumps(vector<int>& arr, int d) {
-        n = arr.size();
-
-        vector<int> dp(n, -1);
-        int ans = 1;
-
-        for(int i = 0; i < n; i++) {
-            ans = max(ans,dfs(i, arr, d, dp));
+        int n = arr.size();
+        vector<int> indices(n);
+        iota(indices.begin(), indices.end(), 0); 
+        sort(indices.begin(), indices.end(), [&](int a, int b) {
+            return arr[a] < arr[b];
+        });
+        
+        vector<int> dp(n, 1);
+        int max_res = 1;
+        for (int i : indices) {
+            for (int j = i - 1; j >= max(0, i - d) && arr[j] < arr[i]; --j) {
+                dp[i] = max(dp[i], 1 + dp[j]);
+            }
+            for (int j = i + 1; j <= min(n - 1, i + d) && arr[j] < arr[i]; ++j) {
+                dp[i] = max(dp[i], 1 + dp[j]);
+            }
+            max_res = max(max_res, dp[i]);
         }
-
-        return ans;
+        
+        return max_res;
     }
 };
